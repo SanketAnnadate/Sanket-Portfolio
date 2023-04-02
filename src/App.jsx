3f61d695bd232a91cb58c3e6032e9895dec35256
SanektAnnadate <sanketannadatejain@gmail.com>
2023-04-02 16:27:39 +0530

    Contact, Earth and Stars components are completed

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ const App = () => {
         <Feedbacks />
         <div className="relative z-0">
           <Contact />
+          <StarsCanvas/>
         </div>
       </div>
     </BrowserRouter>
